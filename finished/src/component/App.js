40d9c496bf6d168798c857e899d7cd3801f74697
Austin Akers <austin.akers5@gmail.com>
2019-11-12 14:12:26 -0600

    pushing finished starter and finished project

--- finished/src/component/App.js
+++ finished/src/component/App.js
@@ -9,7 +9,7 @@ const App = () => {
       <div className="container mx-auto px-4">
         <div>
           <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded m-1" onClick={() => setResource('BboyAkers')}>Austin Akers</button>
-          <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded m-1" onClick={() => setResource('sdras')}>Sarah Drasner</button>
+          <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded m-1" onClick={() => setResource('gaearon')}>Dan Abramov</button>
         </div>
         <RepoList resource={resource} />
       </div>
