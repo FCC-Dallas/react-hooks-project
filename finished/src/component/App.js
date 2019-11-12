7868df91eb159af424a380dd24047d2bd7755619
Austin Akers <austin.akers5@gmail.com>
2019-11-12 12:10:07 -0600

    finished repo finished

--- finished/src/component/App.js
+++ finished/src/component/App.js
@@ -6,10 +6,10 @@ const App = () => {
   //Returns array of two elements inside of it. Similar to destructuring
   const [resource, setResource] = useState('BboyAkers');
     return (
-      <div>
+      <div className="container mx-auto px-4">
         <div>
-          <button onClick={() => setResource('BboyAkers')}>Austin Akers</button>
-          <button onClick={() => setResource('sdras')}>Sarah Drasner</button>
+          <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded m-1" onClick={() => setResource('BboyAkers')}>Austin Akers</button>
+          <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded m-1" onClick={() => setResource('sdras')}>Sarah Drasner</button>
         </div>
         <RepoList resource={resource} />
       </div>
--- finished/src/component/RepoList.js
+++ finished/src/component/RepoList.js
@@ -11,7 +11,24 @@ const RepoList = ({ resource }) => {
     fetchResource(resource);
   }, [resource])
     return (
-      <ul>{resources.map(record => <li key={record.id}>{record.name}</li>)}</ul>
+        <div className="max-w-sm rounded overflow-hidden shadow-lg">
+          {resources.length > 0 ? <img class="w-full" src={resources[0].owner.avatar_url}/> : ''}
+          <div className="px-6 py-4">
+            <div className="font-bold text-xl mb-2">{resource} Repos</div>
+            <p className="text-gray-700 text-base">
+              List of Repos Below:
+            </p>
+          </div>
+          <div className="px-6 py-4">
+          <ul className="px-6 py-4">
+            {resources.map(record =>
+              <li key={record.id} className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 m-1">
+                {record.name}
+              </li>
+            )}
+          </ul>
+          </div>
+        </div>
     )
 }
 
